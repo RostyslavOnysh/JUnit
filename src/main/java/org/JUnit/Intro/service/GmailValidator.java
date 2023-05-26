28946558eb6021b358874b17377d431234670417
Ros <rospsix@gmail.com>
2023-05-26 12:40:50 +0100

    # 3 theory

--- src/main/java/org/JUnit/Intro/service/GmailValidator.java
+++ src/main/java/org/JUnit/Intro/service/GmailValidator.java
@@ -3,6 +3,11 @@ package org.JUnit.Intro.service;
 public class GmailValidator implements EmailValidator{
     @Override
     public boolean isValid(String email) {
-        return false;
+        if (!Character.isLetter(email.charAt(0))){
+            return false;
+        }
+        return true;
     }
+
+
 }
